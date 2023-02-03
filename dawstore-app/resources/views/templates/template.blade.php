3e357cbd5cef04e8ffb5a49e9bc1cb9f821d120a
*Mario <*mariojra6@gmail.com*>
2023-02-03 09:46:15 +0100

    Archivos actualizados

---
+++ dawstore-app/resources/views/templates/template.blade.php
@@ -0,0 +1,12 @@
+@extends('templates.general');
+
+@section('preload')
+<div class="preloader">
+    <div class="preloader-inner">
+        <div class="preloader-icon">
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+</div>
+@endsection
