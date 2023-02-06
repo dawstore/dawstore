8b35247ee10b89f11e2232f5522dd7cd7fcb7b35
ViictorTorres21 <victor.torres.ortiz.18@gmail.com>
2023-02-06 10:23:24 +0100

    subida

--- dawstore-app/resources/views/index.blade.php
+++ dawstore-app/resources/views/index.blade.php
@@ -10,11 +10,11 @@
     <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.svg" />
 
     <!-- ========================= CSS here ========================= -->
-    <link rel="stylesheet" href="../assets/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="../assets/css/LineIcons.3.0.css" />
-    <link rel="stylesheet" href="../assets/css/tiny-slider.css" />
-    <link rel="stylesheet" href="../assets/css/glightbox.min.css" />
-    <link rel="stylesheet" href="../assets/css/main.css" />
+    <link rel="stylesheet" href="../../public/assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="../../public/assets/css/LineIcons.3.0.css" />
+    <link rel="stylesheet" href="../../public/assets/css/tiny-slider.css" />
+    <link rel="stylesheet" href="../../public/assets/css/glightbox.min.css" />
+    <link rel="stylesheet" href="../../public/assets/css/main.css" />
 
 </head>
 
