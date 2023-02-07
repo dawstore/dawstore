2e4057c1e999b4579beceeea3d13ec67ac293314
ViictorTorres21 <victor.torres.ortiz.18@gmail.com>
2023-02-07 10:01:29 +0100

    Actualización

--- dawstore-app/resources/views/templates/general.blade.php
+++ dawstore-app/resources/views/templates/general.blade.php
@@ -12,11 +12,21 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@300;400;700&amp;display=swap">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Martel+Sans:wght@300;400;800&amp;display=swap">
 
-    <!-- theme stylesheet-->
-    @vite(['css/style.default.css','css/custom.css','js/app.js', 'vendor/choices.js/public/assets/styles/choices.min.css', 'vendor/glightbox/css/glightbox.min.css', 'vendor/nouislider/nouislider.min.css',
-    'vendor/swiper/swiper-bundle.min.css', 'vendor/glightbox/js/glightbox.min.js','vendor/nouislider/nouislider.min.js', 'vendor/swiper/swiper-bundle.min.js', 'vendor/choices.js/public/assets/scripts/choices.min.js', 'js/front.js', 'js/icon-svg.js' ,'js/app.js'])
-
-    <link rel="stylesheet" href="css/style.default.css" id="theme-stylesheet">
+     <!-- gLightbox gallery-->
+     <link rel="stylesheet" href="vendor/glightbox/css/glightbox.min.css">
+     <!-- Range slider-->
+     <link rel="stylesheet" href="vendor/nouislider/nouislider.min.css">
+     <!-- Choices CSS-->
+     <link rel="stylesheet" href="vendor/choices.js/public/assets/styles/choices.min.css">
+     <!-- Swiper slider-->
+     <link rel="stylesheet" href="vendor/swiper/swiper-bundle.min.css">
+     <!-- Google fonts-->
+     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@300;400;700&amp;display=swap">
+     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Martel+Sans:wght@300;400;800&amp;display=swap">
+     <!-- theme stylesheet-->
+     <link rel="stylesheet" href="css/style.default.css" id="theme-stylesheet">
+     <!-- Custom stylesheet - for your changes-->
+     <link rel="stylesheet" href="css/custom.css">
     <!-- Favicon-->
     <link rel="shortcut icon" href="img/favicon.png">
 
@@ -36,7 +46,12 @@
 
       <!-- JavaScript files-->
       <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
+      <script src="vendor/glightbox/js/glightbox.min.js"></script>
+      <script src="vendor/nouislider/nouislider.min.js"></script>
+      <script src="vendor/swiper/swiper-bundle.min.js"></script>
+      <script src="vendor/choices.js/public/assets/scripts/choices.min.js"></script>
+      <script src="js/front.js"></script>
+      <script src="js/icon-svg.js"></script>
       <!-- FontAwesome CSS - loading as last, so it doesn't block rendering-->
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     </div>
