e44f0574045989b06e10579cda46541ba1d56423
ViictorTorres21 <victor.torres.ortiz.18@gmail.com>
2023-02-07 09:48:36 +0100

    Implementación Vite

--- dawstore-app/resources/views/templates/general.blade.php
+++ dawstore-app/resources/views/templates/general.blade.php
@@ -3,71 +3,40 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Tello Store | Home</title>
+    <title>Tello Store</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="all,follow">
-    <!-- gLightbox gallery-->
-    <link rel="stylesheet" href="vendor/glightbox/css/glightbox.min.css">
-    <!-- Range slider-->
-    <link rel="stylesheet" href="vendor/nouislider/nouislider.min.css">
-    <!-- Choices CSS-->
-    <link rel="stylesheet" href="vendor/choices.js/public/assets/styles/choices.min.css">
-    <!-- Swiper slider-->
-    <link rel="stylesheet" href="vendor/swiper/swiper-bundle.min.css">
+
     <!-- Google fonts-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@300;400;700&amp;display=swap">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Martel+Sans:wght@300;400;800&amp;display=swap">
+
     <!-- theme stylesheet-->
+    @vite(['css/style.default.css','css/custom.css','js/app.js', 'vendor/choices.js/public/assets/styles/choices.min.css', 'vendor/glightbox/css/glightbox.min.css', 'vendor/nouislider/nouislider.min.css',
+    'vendor/swiper/swiper-bundle.min.css', 'vendor/glightbox/js/glightbox.min.js','vendor/nouislider/nouislider.min.js', 'vendor/swiper/swiper-bundle.min.js', 'vendor/choices.js/public/assets/scripts/choices.min.js', 'js/front.js', 'js/icon-svg.js' ,'js/app.js'])
+
     <link rel="stylesheet" href="css/style.default.css" id="theme-stylesheet">
-    <!-- Custom stylesheet - for your changes-->
-    <link rel="stylesheet" href="css/custom.css">
     <!-- Favicon-->
     <link rel="shortcut icon" href="img/favicon.png">
+
   </head>
   <body>
       <div class="page-holder">
 
         @yield('navbar')
-      
+
         @yield('modal')
-      
+
         @yield('hero_section')
 
         @yield('checkout_section')
-      
+
         @yield('footer')
-            
+
       <!-- JavaScript files-->
       <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-      <script src="vendor/glightbox/js/glightbox.min.js"></script>
-      <script src="vendor/nouislider/nouislider.min.js"></script>
-      <script src="vendor/swiper/swiper-bundle.min.js"></script>
-      <script src="vendor/choices.js/public/assets/scripts/choices.min.js"></script>
-      <script src="js/front.js"></script>
-      <script src="js/icon-svg.js"></script>
-      {{-- <script>
-        // ------------------------------------------------------- //
-        //   Inject SVG Sprite -
-        //   see more here
-        //   https://css-tricks.com/ajaxing-svg-sprite/
-        // ------------------------------------------------------ //
-        function injectSvgSprite(path) {
 
-            var ajax = new XMLHttpRequest();
-            ajax.open("GET", path, true);
-            ajax.send();
-            ajax.onload = function(e) {
-            var div = document.createElement("div");
-            div.className = 'd-none';
-            div.innerHTML = ajax.responseText;
-            document.body.insertBefore(div, document.body.childNodes[0]);
-            }
-        }
-        // ICONOS SVG
-        injectSvgSprite('https://bootstraptemple.com/files/icons/orion-svg-sprite.svg');
-
-      </script> --}}
       <!-- FontAwesome CSS - loading as last, so it doesn't block rendering-->
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     </div>
--- dawstore-app/vite.config.js
+++ dawstore-app/vite.config.js
@@ -4,7 +4,22 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+            'css/style.default.css',
+            'css/custom.css',
+            'js/app.js',
+            'vendor/choices.js/public/assets/styles/choices.min.css',
+            'vendor/glightbox/css/glightbox.min.css',
+            'vendor/nouislider/nouislider.min.css',
+            'vendor/swiper/swiper-bundle.min.css',
+            'vendor/bootstrap/js/bootstrap.bundle.min.js',
+            'vendor/glightbox/js/glightbox.min.js',
+            'vendor/nouislider/nouislider.min.js',
+            'vendor/swiper/swiper-bundle.min.js',
+            'vendor/choices.js/public/assets/scripts/choices.min.js',
+            'js/front.js',
+            'js/icon-svg.js' ,
+            'js/app.js'],
             refresh: true,
         }),
     ],
