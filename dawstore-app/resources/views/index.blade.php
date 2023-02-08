f08e4d47eeec6485ba1d44df5b40c30768c1c6d8
Oscar Povea <oscaarpoveaa@gmail.com>
2023-02-08 12:17:20 +0100

    Cambiado y funcionando el modal

--- dawstore-app/resources/views/index.blade.php
+++ dawstore-app/resources/views/index.blade.php
@@ -53,7 +53,7 @@ active
                     <ul class="mb-0 list-inline">
                       <li class="list-inline-item m-0 p-0"><a class="btn btn-sm btn-outline-dark" href="#!"><i class="far fa-heart"></i></a></li>
                       <li class="list-inline-item m-0 p-0"><a class="btn btn-sm btn-warning" href="cart.html">Add to cart</a></li>
-                      <li class="list-inline-item me-0"><a class="btn btn-sm btn-outline-dark" href="#productView" data-bs-toggle="modal"><i class="fas fa-expand"></i></a></li>
+                      <li class="list-inline-item me-0"><a class="btn btn-sm btn-outline-dark" href="#productView{{$product->id}}" data-bs-toggle="modal"><i class="fas fa-expand"></i></a></li>
                     </ul>
                   </div>
                 </div>
@@ -61,6 +61,46 @@ active
                 <p class="small text-muted">${{ $product->price }}</p>
               </div>
             </div>
+            <!--  Modal -->
+                              <div class="modal fade" id="productView{{$product->id}}" tabindex="-1">
+                                <div class="modal-dialog modal-lg modal-dialog-centered">
+                                  <div class="modal-content overflow-hidden border-0">
+                                    <button class="btn-close p-4 position-absolute top-0 end-0 z-index-20 shadow-0" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    <div class="modal-body p-0">
+                                      <div class="row align-items-stretch">
+                                        <div class="col-lg-6 p-lg-0"><a class="glightbox product-view d-block h-100 bg-cover bg-center" href="img/product-5.jpg" data-gallery="gallery1" data-glightbox="Red digital smartwatch"><img class="img-fluid w-100" src="img/products/products({{$product->sku}}).jpg" alt="..."></a><a class="glightbox d-none" href="img/product-5-alt-1.jpg" data-gallery="gallery1" data-glightbox="Red digital smartwatch"></a><a class="glightbox d-none" href="img/product-5-alt-2.jpg" data-gallery="gallery1" data-glightbox="Red digital smartwatch"></a></div>
+                                        <div class="col-lg-6">
+                                          <div class="p-4 my-md-4">
+                                            <ul class="list-inline mb-2">
+                                              <li class="list-inline-item m-0"><i class="fas fa-star small text-warning"></i></li>
+                                              <li class="list-inline-item m-0 1"><i class="fas fa-star small text-warning"></i></li>
+                                              <li class="list-inline-item m-0 2"><i class="fas fa-star small text-warning"></i></li>
+                                              <li class="list-inline-item m-0 3"><i class="fas fa-star small text-warning"></i></li>
+                                              <li class="list-inline-item m-0 4"><i class="fas fa-star small text-warning"></i></li>
+                                            </ul>
+                                            <h2 class="h4">{{ $product->name }}</h2>
+                                            <p class="text-muted">${{ $product->price }}</p>
+                                            <p class="text-sm mb-4">{{ $product->description }}</p>
+                                            <div class="row align-items-stretch mb-4 gx-0">
+                                              <div class="col-sm-7">
+                                                <div class="border d-flex align-items-center justify-content-between py-1 px-3"><span class="small text-uppercase text-gray mr-4 no-select">Quantity</span>
+                                                  <div class="quantity">
+                                                    <button class="dec-btn p-0"><i class="fas fa-caret-left"></i></button>
+                                                    <input class="form-control border-0 shadow-0 p-0" type="text" value="1">
+                                                    <button class="inc-btn p-0"><i class="fas fa-caret-right"></i></button>
+                                                  </div>
+                                                </div>
+                                              </div>
+                                              <div class="col-sm-5"><a class="btn btn-dark btn-sm w-100 h-100 d-flex align-items-center justify-content-center px-0" href="cart.html">Add to cart</a></div>
+                                            </div><a class="btn btn-link text-dark text-decoration-none p-0" href="#!"><i class="far fa-heart me-2"></i>Add to wish list</a>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+
             @endforeach
         </section>
         <!-- SERVICES-->
