f193d10e26814164de72e02f7fcf679375f1bb4f
*Mario <*mariojra6@gmail.com*>
2023-02-14 12:48:26 +0100

    insert automatico

--- dawstore-app/database/seeders/DatabaseSeeder.php
+++ dawstore-app/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\Product;
+use App\Models\Image;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,12 +16,39 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-        // \App\Models\User::factory(10)->create();
+    {   
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $sku = ['1','2','3','4','5','6','7','8','9','10','11'];
+        $name = ['Adidas Forum','Adidas Yeezy Foam','Air Force 1','Nike Blazer','Nike Air Max 97 Premium','Nike Air Vapormax','New Balance 550','Adidas Yeezy','New Balance 530','Adidas All Star','Jordan 4'];
+        $price = ['100','100','100','100','100','100','100','100','100','100','100'];
+        $description = ['Description 1','Description 2','Description 3','Description 4','Description 5','Description 6','Description 7','Description 8','Description 9','Description 10','Description 11'];
+        $stock = ['50','50','50','50','50','50','50','50','50','50','50'];
+        $genre = ['Male','Unisex','Female','Male','Unisex','Female','Male','Unisex','Female','Male','Unisex'];
+
+
+        $tamaño = sizeof($sku);
+
+        for ($i = 0; $i < $tamaño; $i++) {
+            DB::table('products')->insert([
+                'sku' => $sku[$i],
+                'name' => $name[$i],
+                'price' => $price[$i],
+                'description' => $description[$i],
+                'stock' => $stock[$i],
+                'genre' => $genre[$i]
+            ]);
+        }
+
+        for ($i = 0; $i < $tamaño; $i++) {
+            
+            $images = [$sku[$i].'-1.jpg',$sku[$i].'-2.jpg',$sku[$i].'-3.jpg',$sku[$i].'-4.jpg'];
+    
+            foreach ($images as $image) {
+                DB::table('images')->insert([
+                    'image_name' => $image,
+                    'product_id' => $i+1
+                ]);
+            }
+        }
     }
 }
