67adf050f6de7aae2086fa2e7df3eb1f7020686f
ViictorTorres21 <victor.torres.ortiz.18@gmail.com>
2023-02-15 12:41:00 +0100

    Onmouse

--- dawstore-app/resources/views/detail.blade.php
+++ dawstore-app/resources/views/detail.blade.php
@@ -17,7 +17,9 @@
               <div class="swiper-wrapper">
 
                 @foreach ($product->images as $image )
-                <div class="swiper-slide h-auto swiper-thumb-item mb-3"><img class="w-100" src="{{URL::asset('img/products/'.$product->sku.'-files/'.$image->image_name)}}" alt="..."></div>
+                <div class="swiper-slide h-auto swiper-thumb-item mb-3">
+                    <img class="w-100" src="{{URL::asset('img/products/'.$product->sku.'-files/'.$image->image_name)}}">
+                </div>
                 @endforeach
               </div>
             </div>
@@ -26,7 +28,9 @@
             <div class="swiper product-slider">
               <div class="swiper-wrapper">
                 @foreach ($product->images as $image )
-                <div class="swiper-slide h-auto swiper-thumb-item mb-3"><img class="w-100" src="{{URL::asset('img/products/'.$product->sku.'-files/'.$image->image_name)}}" alt="..."></div>
+                <div class="swiper-slide h-auto swiper-thumb-item mb-3">
+                    <img class="w-100" src="{{URL::asset('img/products/'.$product->sku.'-files/'.$image->image_name)}}" alt="...">
+                </div>
                 @endforeach
               </div>
             </div>
@@ -54,7 +58,9 @@
               </div>
             </div>
           </div>
-          <div class="col-sm-3 pl-sm-0"><a class="btn btn-dark btn-sm btn-block h-100 d-flex align-items-center justify-content-center px-0" href="cart.html">Add to cart</a></div>
+          <div class="col-sm-3 pl-sm-0">
+            <a class="btn btn-dark btn-sm btn-block h-100 d-flex align-items-center justify-content-center px-0" href="cart.html">Add to cart</a>
+        </div>
         </div><a class="text-dark p-0 mb-4 d-inline-block" href="#!"><i class="far fa-heart me-2"></i>Add to wish list</a><br>
         <ul class="list-unstyled small d-inline-block">
           <li class="px-3 py-2 mb-1 bg-white"><strong class="text-uppercase">SKU:</strong><span class="ms-2 text-muted">{{$product->sku}}</span></li>
--- dawstore-app/resources/views/index.blade.php
+++ dawstore-app/resources/views/index.blade.php
@@ -71,7 +71,9 @@ active
             <div class="col-xl-3 col-lg-4 col-sm-6" data-aos="flip-down" data-aos-duration="1500">
                 <div class="product text-center">
                     <div class="position-relative mb-3">
-                        <div class="badge text-white bg-"></div><a class="d-block" href="{{ route('detail', $product) }}"><img class="img-fluid w-100" src="{{URL::asset('img/products/'.$product->sku.'-files/'. $product->images[0]->image_name)}}" alt="..." loading="lazy"></a>
+                        <div class="badge text-white bg-">
+
+                        </div><a class="d-block" href="{{ route('detail', $product) }}"><img class="img-fluid w-100" src="{{URL::asset('img/products/'.$product->sku.'-files/'. $product->images[0]->image_name)}}" alt="..." loading="lazy"></a>
                         <div class="product-overlay">
                             <ul class="mb-0 list-inline">
                                 <li class="list-inline-item m-0 p-0"><a class="btn btn-sm btn-outline-dark" href="#!"><i class="far fa-heart"></i></a></li>
