4327ac6b7b8d5478b61bbc99e22e501d469a1ab6
Victor Torres Ortiz <victor.torres.ortiz.18@gmail.com>
2023-02-16 17:19:51 +0100

    Comenzando Nuevo Login

--- dawstore-app/resources/views/layouts/app.blade.php
+++ dawstore-app/resources/views/layouts/app.blade.php
@@ -5,62 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Tello Store | Login</title>
+    <title>Tello Store | Register</title>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <!-- Styles -->
-    {{-- @vite(['resources/js/app.js', 'resources/css/app.scss']) --}}
-    <link rel="stylesheet" href="{{URL::asset('/css/login.css')}}">
+    {{-- @vite(['resources/js/app.js', 'resources/css/app.scss', 'public/css/register.css', 'public/js/register.js']) --}}
+    <link rel="stylesheet" href="{{URL::asset('css/register.css')}}">
     <link rel="shortcut icon" href="{{URL::asset('img/logos/logoTello.png')}}">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+
+
 </head>
 <body>
-    <div id="app">
-        {{-- <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Diario del Programador') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <div class="dropdown">
-                                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                                    {{ Auth::user()->name }}
-                                </button>
-                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                  <li><a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a></li>
-                                </ul>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav> --}}
-        <main class="pt-4">
-            @yield('content')
-        </main>
-    </div>
 
-    <script src="{{URL::asset('js/login.js')}}"></script>
+        @yield('content')
+    
+
+    <script src="{{URL::asset('js/register.js')}}"></script>
 </body>
 </html>
