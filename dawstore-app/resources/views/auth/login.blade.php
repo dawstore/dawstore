19699912a2257afd12296a09e28da6e9f0a52792
ViictorTorres21 <victor.torres.ortiz.18@gmail.com>
2023-02-22 10:35:29 +0100

    login y register

--- dawstore-app/resources/views/auth/login.blade.php
+++ dawstore-app/resources/views/auth/login.blade.php
@@ -5,11 +5,11 @@ Login
 @endsection
 
 @section('content')
-<div class="container">
+<div class="container border-5">
   <div class="row justify-content-center">
       <div class="col-md-8">
           <div class="card">
-              <div class="card-header">{{ __('Login') }}</div>
+              <div class="card-header justify-content-center d-flex h1 transparent">{{ __('Login Acount') }}</div>
               <div class="card-body">
                   <form method="POST" action="{{ route('login') }}">
                       @csrf
