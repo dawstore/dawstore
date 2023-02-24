bd8f9446f74454fe0a7641f933976a911895ae6e
Mario Rufo Ariza <mariojra6@gmail.com>
2023-02-24 02:23:19 +0100

    login y register

--- dawstore-app/resources/views/profile.blade.php
+++ dawstore-app/resources/views/profile.blade.php
@@ -85,15 +85,6 @@ Profile
                         <hr>
                         <div class="row">
                             <div class="col-sm-3">
-                                <p class="mb-0">Country</p>
-                            </div>
-                            <div class="col-sm-9">
-                                <p class="text-muted mb-0">{{Auth::user()->country}}</p>
-                            </div>
-                        </div>
-                        <hr>
-                        <div class="row">
-                            <div class="col-sm-3">
                                 <p class="mb-0">Phone</p>
                             </div>
                             <div class="col-sm-9">
