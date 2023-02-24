d9163fe6986059dc57f9b7bfab67096aa699c68e
*Mario <*mariojra6@gmail.com*>
2023-02-24 09:41:36 +0100

    admin

--- dawstore-app/app/Http/Controllers/AdminpanelController.php
+++ dawstore-app/app/Http/Controllers/AdminpanelController.php
@@ -74,66 +74,66 @@ class AdminpanelController extends Controller
 
     public function update(Request $request, $id)
     {
-        // $file_path = Product::findOrFail($id);
+        $file_path = Product::findOrFail($id);
 
-        // if (File::exists(public_path("img/products/" . $file_path->images))) {
-        //     File::delete(public_path("img/products/" . $file_path->images));
-        // }
-
-        // $productUpdate = Product::findOrFail($id);
-        // $productUpdate->sku = $request->sku;
-        // $productUpdate->name = $request->name;
-        // $productUpdate->price = $request->price;
-        // $productUpdate->description = $request->description;
-        // $productUpdate->stock = $request->stock;
-        // $productUpdate->genre = $request->genre;
-        // if($request->hasFile("images")){
-        //     $imagen = $request->images;
-        //     $nombreimagen = Str::slug($request->name).".".$imagen->guessExtension();
-        //     $ruta = public_path("img/products/");
-        //     $imagen->move($ruta,$nombreimagen);
-        //     $productUpdate->images = $nombreimagen;
-        // }
-        // $productUpdate->update();
-        // smilify('success', 'Product edit successfully!');
-        // return back();
-
-        $productsImages = count($request->images);
-
-        if ($productsImages < 4) {
-            smilify('error', 'You have to upload 4 images to create the product!');
-            return back();
-        } else if ($productsImages > 4) {
-            smilify('error', 'You have to upload 4 images to create the product!');
-            return back();
-        } else {
-            $productUpdate = Product::findOrFail($id);
-            $productUpdate->sku = $request->sku;
-            $productUpdate->name = $request->name;
-            $productUpdate->price = $request->price;
-            $productUpdate->description = $request->description;
-            $productUpdate->stock = $request->stock;
-            $productUpdate->genre = $request->genre;
-            if($request->hasFile("images")){
-                $file_path = Product::findOrFail($id);
-                if (File::exists(public_path("img/products/" . $file_path->images))) {
-                    File::delete(public_path("img/products/" . $file_path->images));
-                }
-                $i = 1;
-                foreach ($request->images as $image) {
-                $imagen = new Image();
-                $imagen->product_id = $productUpdate->id;
-                $imagen->image_name = $request->sku."-".$i.".".$image->extension();
-                $ruta = public_path("img/products/".$request->sku."-files");
-                $image->move($ruta,$imagen->image_name);
-                $imagen->save();
-                $i++;
-                }
-            }
-            $productUpdate->update();
-            smilify('success', 'Product edit successfully!');
-            return back();
+        if (File::exists(public_path("img/products/" . $file_path->images))) {
+            File::delete(public_path("img/products/" . $file_path->images));
         }
+
+        $productUpdate = Product::findOrFail($id);
+        $productUpdate->sku = $request->sku;
+        $productUpdate->name = $request->name;
+        $productUpdate->price = $request->price;
+        $productUpdate->description = $request->description;
+        $productUpdate->stock = $request->stock;
+        $productUpdate->genre = $request->genre;
+        if($request->hasFile("images")){
+            $imagen = $request->images;
+            $nombreimagen = Str::slug($request->name).".".$imagen->guessExtension();
+            $ruta = public_path("img/products/");
+            $imagen->move($ruta,$nombreimagen);
+            $productUpdate->images = $nombreimagen;
+        }
+        $productUpdate->update();
+        smilify('success', 'Product edit successfully!');
+        return back();
+
+        // $productsImages = count($request->images);
+
+        // if ($productsImages < 4) {
+        //     smilify('error', 'You have to upload 4 images to create the product!');
+        //     return back();
+        // } else if ($productsImages > 4) {
+        //     smilify('error', 'You have to upload 4 images to create the product!');
+        //     return back();
+        // } else {
+        //     $productUpdate = Product::findOrFail($id);
+        //     $productUpdate->sku = $request->sku;
+        //     $productUpdate->name = $request->name;
+        //     $productUpdate->price = $request->price;
+        //     $productUpdate->description = $request->description;
+        //     $productUpdate->stock = $request->stock;
+        //     $productUpdate->genre = $request->genre;
+        //     if($request->hasFile("images")){
+        //         $file_path = Product::findOrFail($id);
+        //         if (File::exists(public_path("img/products/" . $file_path->images))) {
+        //             File::delete(public_path("img/products/" . $file_path->images));
+        //         }
+        //         $i = 1;
+        //         foreach ($request->images as $image) {
+        //         $imagen = new Image();
+        //         $imagen->product_id = $productUpdate->id;
+        //         $imagen->image_name = $request->sku."-".$i.".".$image->extension();
+        //         $ruta = public_path("img/products/".$request->sku."-files");
+        //         $image->move($ruta,$imagen->image_name);
+        //         $imagen->save();
+        //         $i++;
+        //         }
+        //     }
+        //     $productUpdate->update();
+        //     smilify('success', 'Product edit successfully!');
+        //     return back();
+        // }
     }
 
 
